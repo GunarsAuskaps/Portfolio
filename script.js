b5a9b7e6009d1817bf28c1df497ea1be5064dedc
GunarsAuskaps <51334984+GunarsAuskaps@users.noreply.github.com>
2019-09-16 18:36:45 -0700

    Updated JS.
    Removed .jumper, and made a NavBar.

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-$(".jumper").on("click", function( e ) {
-
-    
-
-    e.preventDefault();
-
-
-
-    $("body, html").animate({ 
-
-        scrollTop: $( $(this).attr('href') ).offset().top 
-
-    }, 600);
-
-    
-
-});
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
